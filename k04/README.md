8348f0fd739855e25fb3f4ba01d83ec7d90af78a
kiuchi-shuta <bb35319018@ms.nagasaki-u.ac.jp>
2020-07-22 00:53:47 +0900

    課題提出

--- k04/README.md
+++ k04/README.md
@@ -1,6 +1,6 @@
 # 課題4 レポート
 
-aa83988848 薗田光太郎
+bb35319018 木内柊汰
 
 ## 課題
 
@@ -11,7 +11,30 @@ aa83988848 薗田光太郎
 この二つのファイルを読み取り，学籍番号，身長，性別の3つをメンバーとする構造体の配列で管理するとよい．
 
 ## ソースコードの説明
+6:構造体を定義
+23: データファイル名を入力させ，fnameに格納
+24: 入力させた文字列の最後にエンターキーが入っているので，fnameの文字列の最後をヌル文字'\0'に置換
+25: fnameを確認
+27: fnameで指定したファイルの先頭アドレスをファイルポインタ fp に読み込む
+28: もしfpがNULLだったときは，ファイルを開けなかったので，即座にプログラム終了
+33: ファイルを1行ずつbufに読み込む．もし読み込み成功する限り以下を繰り返す
+38: bufからscanf身長と性別を読み込む
+39: 数値の個数をカウント
+42: ファイルを閉じる．閉じれないときは，即座にプログラム終了.
+48: データファイル名を入力させ，fnameに格納
+49: 入力させた文字列の最後にエンターキーが入っているので，fnameの文字列の最後をヌル文字'\0'に置換
+50: fnameを確認
+52: fnameで指定したファイルの先頭アドレスをファイルポインタ fp に読み込む
+53: もしfpがNULLだったときは，ファイルを開けなかったので，即座にプログラム終了
+59: ファイルを1行ずつbufに読み込む．もし読み込み成功する限り以下を繰り返す
+60: bufからscanfでidを読み込む
+61: 数値の個数をカウント
+63: ファイルを閉じる．閉じれないときは，即座にプログラム終了.
+68:idを入力させる
+71:idがあるものが入力された場合、性別が男性か女性かを判別し、男性の場合male、女性の場合femaleと表示し慎重を表示
+89:idがない場合NO dataと表示
 
+なぜNo　dataと出ないかわかりません
 
 ## 入出力結果
 
@@ -36,6 +59,24 @@ Which ID's data do you want? : 45313124
 ---
 No data
 ```
+PS C:\Users\bb35319018\新しいフォルダー (7)\2020psp2\k04> ./k04
+Input the filename of sample height:c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\heights.csv
+the filename of sample: c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\heights.csv
+Input the filename of sample ID:c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\IDs.csv
+the filename of sample: c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\IDs.csv
+Which ID's data do you want? :45313125
+ID : 45313125
+gender : female
+height : 152.4
+PS C:\Users\bb35319018\新しいフォルダー (7)\2020psp2\k04> ./k04
+Input the filename of sample height:c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\heights.csv
+the filename of sample: c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\heights.csv
+Input the filename of sample ID:c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\IDs.csv
+the filename of sample: c:\Users\bb35319018\新しいフォルダー (7)\2020psp2\sample\IDs.csv
+Which ID's data do you want? :45313124
+No data
+PS C:\Users\bb35319018\新しいフォルダー (7)\2020psp2\k04>
+
 
 ## 修正履歴
 
--- k04/k04.c
+++ k04/k04.c
@@ -1,1 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+typedef struct{
+     int id;
+     int gender;
+     double height;
+     
+} studentdata;
+
+int main(void)
+{
+    studentdata data[14];
+    char fname[FILENAME_MAX];
+    FILE* fp;
+    char buf[256];
+    int i = -1;
+    int idset;
+
+    printf("Input the filename of sample height:");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample: %s\n", fname);
+
+    fp = fopen(fname,"r");
+    if(fp==NULL){
+        fputs("File open error\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+      while(fgets(buf,sizeof(buf),fp) != NULL){
+        if(i == -1){
+            i++;
+            continue;
+        }
+        sscanf(buf,"%d,%lf", &data[i].gender, &data[i].height);
+        i++;
+    }
+
+    if(fclose(fp) == EOF){
+        fputs("file close error\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Input the filename of sample ID:");
+    fgets(fname,sizeof(fname), stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample: %s\n", fname);
+
+    fp = fopen(fname,"r");
+    if(fp==NULL){
+        fputs("file open error\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    i=0;
+    while(fgets(buf,sizeof(buf),fp) != NULL){
+        sscanf(buf,"%d",&data[i].id);
+        i++;
+    }
+    if(fclose(fp) == EOF){
+        fputs("file close error\n", stderr);
+       exit(EXIT_FAILURE);
+    }
+ printf("Which ID's data do you want? :");
+    scanf("%d",&idset);
+
+     
+    for(i = 0; i < 14 ; i++)
+    {
+        if(data[i].id == idset)
+        {
+            printf("ID : %d\n",data[i].id);
+            if(data[i].gender == 1)
+            {
+                printf("gender : male\n");
+            }
+            else
+            {
+                printf("gender : female\n");
+            }
+            printf("height : %.1lf\n",data[i].height);
+            return 0;
+        }
+    }
+   
+     printf("No data\n");
+     return 0;
+}
